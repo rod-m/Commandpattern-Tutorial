17fe5933b9d0f84c173c782ca65dec1975cc51e7
Rod Martin <r.martin1@salford.ac.uk>
2023-01-24 11:59:17 +0000

    Ready to refactor

---
+++ Assets/Scripts/GameTools/MessageSystem/EventManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using Singletons;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace GameTools.MessageSystem
+{
+    public sealed class EventManager : GenericSingleton<EventManager>
+    {
+        private Dictionary<string, UnityEvent> eventDictionary = new Dictionary<string, UnityEvent>();
+        
+        public override void Awake()
+        { 
+            
+            base.Awake();
+          
+        }
+    
+        public void StartListening (string eventName, UnityAction listener)
+        {
+            UnityEvent thisEvent = null;
+            Debug.Log($"StartListen {Instance.name}");
+            if (Instance.eventDictionary.TryGetValue (eventName, out thisEvent))
+            {
+                thisEvent.AddListener (listener);
+            } 
+            else
+            {
+                thisEvent = new UnityEvent ();
+                thisEvent.AddListener (listener);
+                Instance.eventDictionary.Add (eventName, thisEvent);
+            }
+        }
+
+        public void StopListening (string eventName, UnityAction listener)
+        {
+            if (Instance == null) return;
+            UnityEvent thisEvent = null;
+            if (Instance.eventDictionary.TryGetValue (eventName, out thisEvent))
+            {
+                thisEvent.RemoveListener (listener);
+            }
+        }
+
+        public void TriggerEvent (string eventName)
+        {
+            UnityEvent thisEvent = null;
+            if (Instance.eventDictionary.TryGetValue (eventName, out thisEvent))
+            {
+                thisEvent.Invoke ();
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/GameTools/MessageSystem/EventTest.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using GameTools.MessageSystem;
+public class EventTest : MonoBehaviour {
+
+    private UnityAction someListener;
+
+    void Awake ()
+    {
+        someListener = new UnityAction (DoSomething);
+        someListener += SomeOtherFunction;
+    }
+
+    void OnEnable ()
+    {
+        EventManager.Instance.StartListening ("test", someListener);
+        EventManager.Instance.StartListening ("Spawn", SomeOtherFunction);
+        EventManager.Instance.StartListening ("Destroy", SomeThirdFunction);
+    }
+
+    void OnDisable ()
+    {
+        EventManager.Instance.StopListening ("test", someListener);
+        EventManager.Instance.StopListening ("Spawn", SomeOtherFunction);
+        EventManager.Instance.StopListening ("Destroy", SomeThirdFunction);
+    }
+
+    void DoSomething ()
+    {
+        Debug.Log ("Some Function was called!");
+    }
+    
+    void SomeOtherFunction ()
+    {
+        Debug.Log ("Some Other Function was called!");
+    }
+    
+    void SomeThirdFunction ()
+    {
+        Debug.Log ("Some Third Function was called!");
+    }
+}
